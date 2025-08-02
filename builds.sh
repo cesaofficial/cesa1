b0dbb49c80d5aea75a549ac4f71fd2ca13c6f0a3
adityakhebade <adityakhebade9@gmail.com>
2025-08-02 11:47:20 +0530

    staticfiles created vercel and builds.sh updated

--- builds.sh
+++ builds.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
 
-# Activate virtual environment (if needed)
-# source venv/bin/activate
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-# Run migrations and collect static files
+# Install dependencies
+pip install -r requirements.txt
+
+# Run database migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
--- cesa_project/settings.py
+++ cesa_project/settings.py
@@ -143,14 +143,15 @@ USE_TZ = True
 
 # Find the STATIC_URL setting and add these lines below it:
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static") # Vercel will collect static files here
+STATIC_ROOT = BASE_DIR / 'staticfiles' # Vercel will collect static files here
 # settings.py (add this at the end of the static files section)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # This tells Django where to find your static files in development
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, 'core/static'),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
