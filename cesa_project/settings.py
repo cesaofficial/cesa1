886d0051b31e0d58b175cec740b91e3031a5adb1
adityakhebade <adityakhebade9@gmail.com>
2025-08-02 10:36:56 +0530

    setting.py allowed host changes

--- cesa_project/settings.py
+++ cesa_project/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 # --- ALLOWED HOSTS ---
 # This configuration is robust and recommended for Vercel deployments.
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 
