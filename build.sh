5288ef2bf6f7d22e6c58646dac5b51f29adb2c15
adityakhebade <adityakhebade9@gmail.com>
2025-08-04 00:59:32 +0530

    Final Config: Control all build steps via vercel.json

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Exit on error
 set -e
+
+# We must install packages ourselves because the UI is ignored
+python3.9 -m pip install -r requirements.txt
+
+# Run migrations
 python3.9 manage.py migrate
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
