c416891747ffe7eb5f89427eba88aa0c26c207d3
adityakhebade <adityakhebade9@gmail.com>
2025-08-04 22:56:26 +0530

    Revert to vercel.json build config

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ npm install
 npm run build
 cd ..
 
+# Install Python dependencies
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
 # Collect all static files
 echo "Running collectstatic..."
 python manage.py collectstatic --no-input
