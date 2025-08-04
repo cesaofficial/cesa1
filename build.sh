1553d92aae86a1bdb32606889417103ff0e8d9f3
adityakhebade <adityakhebade9@gmail.com>
2025-08-05 02:22:40 +0530

    The definitive and final Vercel configuration

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
-# This line will print every command that is run, for maximum debugging
-set -x
-# This line will exit the script if any command fails
 set -o errexit
 
-echo "--- CHECKING SYSTEM VERSIONS ---"
-python3 --version
-node --version
-npm --version
+# The @vercel/static-build environment is guaranteed to have python3, pip, node, and npm.
+# We use python3 and pip3 to be explicit and avoid any system path issues.
 
 echo "--- INSTALLING ALL DEPENDENCIES ---"
-# Use python3 and pip3 to be explicit
-python3 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 cd frontend
 npm install
 cd ..
@@ -23,12 +16,8 @@ npm run build
 cd ..
 
 echo "--- PREPARING DJANGO DEPLOYMENT FILES ---"
-# Create the staticfiles directory to ensure it exists
 mkdir -p staticfiles
-# Copy the built index.html into the directory that Django will use
 cp frontend/dist/index.html staticfiles/
-
-# Run collectstatic using python3
 python3 manage.py collectstatic --no-input
 
 echo "--- BUILD SCRIPT HAS COMPLETED SUCCESSFULLY ---"
