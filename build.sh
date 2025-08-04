47f852a67fa2221a946b1ad516dead198308a5cf
adityakhebade <adityakhebade9@gmail.com>
2025-08-05 01:59:59 +0530

    Definitive configuration for new Vercel project

--- build.sh
+++ build.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 set -o errexit
 
-# This script will be run by Vercel's "Build Command".
-# The 'pip install' command will be run separately by Vercel's "Install Command".
-
-echo "--- Building frontend ---"
+echo "--- INSTALLING ALL DEPENDENCIES ---"
+pip install -r requirements.txt
 cd frontend
 npm install
+cd ..
+
+echo "--- BUILDING FRONTEND ---"
+cd frontend
 npm run build
 cd ..
 
-echo "--- Preparing static files for Django ---"
-# Create the staticfiles directory to be safe
+echo "--- PREPARING DJANGO DEPLOYMENT FILES ---"
 mkdir -p staticfiles
-# Manually copy the essential index.html to where Django's TEMPLATES setting will find it
 cp frontend/dist/index.html staticfiles/
-
-echo "--- Collecting all static assets ---"
-# Use python3 to be explicit. This command will gather all static files
-# (from Django's admin, and from your frontend's 'dist' folder)
-# into the final 'staticfiles' directory.
 python3 manage.py collectstatic --no-input
 
-echo "--- BUILD SCRIPT COMPLETE ---"
+echo "--- BUILD SCRIPT HAS COMPLETED SUCCESSFULLY ---"
